e401342d17a81282116d55da1366c535ea01e519
Mehmet <mehmetkusken@gmail.com>
2021-03-08 20:16:04 -0700

    commit tree 3ff267cac8df85da7383a5be4e5546db4f6c4054

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,16 @@
 
 # Then any files that need to load the files required here can
 # require THIS file, environment.rb, and get access to everything
+require_relative '../lib/garden.rb'
+require_relative '../lib/plant.rb' 
+require_relative '../config/environment.rb'
+
+lawn = Garden.new(name: 'Front Lawn')
+
+basil = Plant.new(name: 'Basil')
+basil.garden = lawn
+
+cucumber = Plant.new(name: 'Cucumber')
+cucumber.garden = lawn
+
+p lawn.plants 
